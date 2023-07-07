4d980dbaa4a88e072e22788d317232d9c06cfe0e
Ashish <savitayadavmansarover@gmail.com>
2023-07-08 02:29:34 +0530

    frontendtodo

---
+++ src/pages/Register.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { Link, Navigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { Context, server } from "../main";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { isAuthenticated, setIsAuthenticated, loading, setLoading } =
+    useContext(Context);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const data = await axios.post(
+        `${server}user/new`,
+        {
+          name,
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+      setIsAuthenticated(true);
+      toast.success(data.message);
+      setLoading(false);
+    } catch (error) {
+      toast.error(error.response.data.message);
+      setIsAuthenticated(false);
+      setLoading(false);
+    }
+    console.log("Check3");
+  };
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+  return (
+    <div className="login">
+      <section>
+        <form onSubmit={submitHandler}>
+          <input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            placeholder="Name"
+            required
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            required
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            autoComplete="true"
+          />
+          <button disabled={loading} type="submit">
+            Sign Up
+          </button>
+          <h4>Or</h4>
+          <Link to="/login">Log In</Link>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default Register;
